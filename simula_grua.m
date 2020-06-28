12c1a200766ac1efd60ddbd7f9aee8f11e86127b
Gonzalo Cortes <gdcortes@uc.cl>
2020-06-27 20:07:06 -0400

    alfa se mantiene horizontal feedforward

--- simula_grua.m
+++ simula_grua.m
@@ -1,16 +1,17 @@
 close all
-clear
+clear all
 % SIMULACION DE GRUA EN LAZO ABIERTO CON ENTRADA SOLO EN ELEVACION
-x0 = [0 0*pi/180 0 0 0 0];
+x0 = [0*pi/180 0*pi/180 6 0 0 0];
 
 %Tiempo de muestreo por defecto
 Ts = 1;
 
 %Tiempo de simulación
-Tmax = 40;
+Tmax = 1000;
 
 %Torques y fuerza iniciales
-Ta = 0;
+Ta = 9.82*cos(x0(1)) * (1000*5 + 600*(10+6/2)+500*(10+6));
+    
 Tb = 0;
 Ff = 0;
 
@@ -30,6 +31,17 @@ Kp_beta=170000;
 Kd_beta=1000000;
 Ki_beta=5000;
 k=1;
+
+int_err_alpha=0;
+err_alpha=0;
+ref_alpha=0*pi/180;
+Kp_alpha=0;
+Kd_alpha=0;
+Ki_alpha=0;
+
+error=[];
+
+ref_flecha=6;
 
 %Simulación
 for t_actual=0:Ts:Tmax
@@ -53,46 +65,69 @@ for t_actual=0:Ts:Tmax
     beta_actual=valores(2);
     flecha_actual = valores(3);
     
+    
+    % beta
     err_beta_old=err_beta;
     err_beta=ref_beta-beta_actual;
     
     if k==1, vel_err_beta=0; % velocidad del error (0 para 1a iteracion)
     else vel_err_beta=(err_beta-err_beta_old)/Ts;
     end
-    k=k+1;
+  
    
-    
     int_err_beta=int_err_beta+abs(err_beta);
    
-    if err_beta<100
+    if err_beta<10
         int_err_beta=0;
     end
 
-    
     Tb=Kp_beta*err_beta+Kd_beta*vel_err_beta+Ki_beta*int_err_beta;
     
+    % alpha
+    err_alpha_old=err_alpha;
+    err_alpha=ref_alpha-alpha_actual;
     
-  
-        
+    if k==1, vel_err_alpha=0; % velocidad del error (0 para 1a iteracion)
+    else vel_err_alpha=(err_alpha-err_alpha_old)/Ts;
+    end
+    
+   
+    int_err_alpha=int_err_alpha+abs(err_alpha);
+   
+    if err_alpha<10
+        int_err_alpha=0;
+    end
 
+    Ta=(Kp_alpha*err_alpha+Kd_alpha*vel_err_alpha+Ki_alpha*int_err_alpha)...
+        + 9.82*cos(ref_alpha) * (1000*5 + 600*(10+6/2)+500*(10+6));
+    
+    disp(Ta)
+    
+      
+    
     
     %Ejemplo para almacenar valores
     alpha = [alpha; alpha_actual];
     beta = [beta; beta_actual];
     flecha = [flecha; flecha_actual];
+    error=[error;err_alpha];
+    
+     k=k+1;
     
 end
 
 %Graficando alpha
-% 
-% figure(1)
-% subplot(3,1,1)
-% plot(alpha*180/pi); title('Elevacion [grados]')
-% subplot(3,1,2)
-% plot(beta*180/pi); title('Azimut [grados]') 
-% subplot(3,1,3)
-% plot(flecha);  title('Desplazamiento flecha [m]')
 
-plot(beta*180/pi); title('Azimut [grados]')
+figure(1)
+subplot(3,1,1)
+plot(alpha*180/pi); title('Elevacion [grados]')
+subplot(3,1,2)
+plot(beta*180/pi); title('Azimut [grados]') 
+subplot(3,1,3)
+plot(flecha);  title('Desplazamiento flecha [m]')
+
+ figure(2)
+ plot(error);
+
 
 
